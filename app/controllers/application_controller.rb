09a0c82a93718b72d76e72eadbd1589d995bc266
Ming Qu <qucool@gmail.com>
2015-09-17 21:18:30 +0800

    Fix devise session helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :render_flash?, :enable_render_flash, :disable_render_flash
-  helper_method :wechat_request?
+  helper_method :render_flash?, :enable_render_flash, :disable_render_flash,
+    :current_user, :current_handyman, :user_signed_in?, :handyman_signed_in?, :wechat_request?
 
   private
 
@@ -23,4 +23,21 @@ class ApplicationController < ActionController::Base
   def wechat_request?
     request.env['HTTP_USER_AGENT'].include?(' MicroMessenger/')
   end
+
+  def current_user
+    @current_user ||= current_account if current_account.try(:is_a?, User)
+  end
+
+  def current_handyman
+    @current_handyman ||= current_account if current_account.try(:is_a?, Handyman)
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
+
+  def handyman_signed_in?
+    !!current_handyman
+  end
+
 end
