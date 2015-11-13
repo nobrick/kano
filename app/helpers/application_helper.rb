f6ce9d7c090d2a4b306ae21de54502b50f7a7a12
Ming Qu <qucool@gmail.com>
2015-11-13 12:37:42 +0800

    Set phone_link_for helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_flash_message
     render 'shared/flash_messages' if render_flash?
   end
+
+  def phone_link_for(account)
+    link_to account.readable_phone_number, "tel:#{account.phone}"
+  end
 end
