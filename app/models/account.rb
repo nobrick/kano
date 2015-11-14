f029db615c6e70f8d0abb0627fb3609d2074a2c2
Ming Qu <qucool@gmail.com>
2015-11-15 00:23:21 +0800

    Disable randomizing account id

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,4 @@
 class Account < ActiveRecord::Base
-  include IdRandomizable
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
