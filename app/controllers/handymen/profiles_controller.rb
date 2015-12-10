87be522f240378136dea74348b61468026c9ac50
Ming Qu <qucool@gmail.com>
2015-12-10 20:09:31 +0800

    Fix profile actions authentication issue

--- app/controllers/handymen/profiles_controller.rb
+++ app/controllers/handymen/profiles_controller.rb
@@ -1,4 +1,6 @@
 class Handymen::ProfilesController < ProfilesController
+  before_action :authenticate_completed_handyman, only: [ :show, :edit ]
+
   def show
     @orders = Order.where(handyman: current_handyman)
       .order(updated_at: :desc).limit(2)
