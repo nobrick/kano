5e61b781c85626d72528c7c36f53173180ef4951
Ming Qu <qucool@gmail.com>
2016-02-21 17:49:53 +0800

    Add handyman withdrawal pages

--- app/controllers/handymen/withdrawals_controller.rb
+++ app/controllers/handymen/withdrawals_controller.rb
@@ -1,21 +1,21 @@
 class Handymen::WithdrawalsController < ApplicationController
+  attr_reader :account
   before_action :authenticate_completed_handyman
   before_action :set_account
+  before_action :set_withdrwal_info, only: [ :index, :new ]
 
   # GET /withdrawals
   def index
-    @withdrawals = @account.withdrawals
+    @withdrawals = account.withdrawals
       .order(created_at: :desc)
       .page(params[:page])
       .per(7)
-
-    @withdrawal = @account.withdrawals.build
-    @withdrawal.validate
+    @last_withdrawal_total = @withdrawals.first.try(:total) || 0
   end
 
   # GET /withdrawals/new
   def new
-    @withdrawal = @account.withdrawals.build
+    @withdrawal = account.withdrawals.build
   end
 
   # POST /withdrawals
@@ -24,6 +24,7 @@ class Handymen::WithdrawalsController < ApplicationController
     if @withdrawal.request && @withdrawal.save
       redirect_to handyman_withdrawals_url, notice: t('.requested')
     else
+      set_withdrwal_info
       render :new
     end
   end
@@ -36,5 +37,14 @@ class Handymen::WithdrawalsController < ApplicationController
 
   def set_account
     @account = current_handyman
+  end
+
+  def set_withdrwal_info
+    @balance = account.balance
+    @unfrozen_balance = account.unfrozen_balance
+    @frozen_balance = @balance - @unfrozen_balance
+    @latest_record = account.latest_balance_record
+    @is_today_permitted = Withdrawal.at_permitted_requesting_date?
+    @next_permitted_date = Withdrawal.next_permitted_requesting_date
   end
 end
