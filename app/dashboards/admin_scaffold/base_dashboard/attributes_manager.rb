b8cdbf638b5202a5f21b1f0336105635eef21aa1
hymRedemption <he.redemption@gmail.com>
2016-04-26 10:59:27 +0800

    Add 'AttibutesManager' class to manage the attributes

---
+++ app/dashboards/admin_scaffold/base_dashboard/attributes_manager.rb
@@ -0,0 +1,61 @@
+module AdminScaffold
+  class BaseDashboard
+    class AttributesManager
+      attr_reader :resource_class, :attributes
+      def initialize(resource_class)
+        @resource_class = resource_class
+        @attributes = {}
+      end
+
+      def string(attr_index, options = {})
+        define_attribute(attr_index, Field::String, options)
+      end
+
+      def date_time(attr_index, options = {})
+        define_attribute(attr_index, Field::DateTime, options)
+      end
+
+      def number(attr_index, options = {})
+        define_attribute(attr_index, Field::Number, options)
+      end
+
+      def expand(attr_index, options = {})
+        define_attribute(attr_index, Field::Expand, options)
+      end
+
+      def attr_name(attr_index)
+        @attributes[attr_index].name
+      end
+
+      private
+
+      def define_attribute(attr_index, type, options = {})
+        validate!(attr_index)
+        attr_owner = options.delete(:owner) || @resource_class
+        @attributes[attr_index] = Attribute.new(original_attr(attr_index), attr_owner, type, options)
+      end
+
+      def original_attr(attr)
+        if !(attr =~ /\./)
+          attr
+        else
+          attr.split('.')[-1]
+        end
+      end
+
+      def attr_owner(attr)
+        @attributes[attr][:attr_owner]
+      end
+
+      def validate!(attr)
+        if attr_defined?(attr)
+          raise "This attribute has been defiend"
+        end
+      end
+
+      def attr_defined?(attr)
+        @attributes.has_key?(attr)
+      end
+    end
+  end
+end
